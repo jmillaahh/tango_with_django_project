240672b76be9849849954dbcc563f7f47e4be38d
Josh Miller <2509263m@student.gla.ac.uk>
2020-02-14 15:14:04 +0000

    tests_chapter3.py passed and subsequently deleted

--- rango/views.py
+++ rango/views.py
@@ -3,13 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    # html = html = "Rango says hey there partner!<br/>" + '<a href="/rango/about/">About</a>'
-    # return HttpResponse(html)
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    return render(request, 'rango/index.html', context=context_dict)
+    html = "Rango says hey there partner!<br/>" + '<a href="/rango/about/">About</a>'
+    return HttpResponse(html)
+    # context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    # html = "Rango says here is the about page.<br/>" + '<a href="/rango/">Index</a>'
-    # return HttpResponse(html)
-    return render(request, 'rango/about.html')
-    # comment added to make new commit 
+    html = "Rango says here is the about page.<br/>" + '<a href="/rango/">Index</a>'
+    return HttpResponse(html)
+    # return render(request, 'rango/about.html')
+    # comment added to make new commit
